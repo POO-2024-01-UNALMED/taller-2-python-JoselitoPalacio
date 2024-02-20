8f55db2ced5a764f9be5ea40290b282cb75a88c2
JoselitoPalacio <jpalaciose@unal.edu.co>
2024-02-20 02:33:08 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
-class auto:
+class Auto:
     cantidadcreados =0
+    def __init__(self, modelo, precio, asientos, marca, motor, registro):
+         self.modelo = modelo
+         self.precio= precio
+         self.asientos = asientos
+         self.marca = marca
+         self.motor = motor
+         self.registro = registro
+
+class Motor:
+        def __init__(self, numeroCilindros, tipo, registro):
+          self.numeroCilindros = numeroCilindros
+          self.tipo = tipo
+          self.registro = registro
+
+class Asiento:
+       def __init__(self, color, precio, registro):
+        self.color = color
+        self.precio = precio
+        self.registro = registro
 
